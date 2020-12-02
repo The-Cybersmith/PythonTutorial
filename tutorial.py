d1c5c15eb3d3c07971429bb843318810760e3afd
The-Cybersmith <matthew.c.g.i@hotmail.com>
2020-12-02 20:03:43 +0000

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -1,5 +1,8 @@
 import requests
-import json 
+import json
+from plotly import *
+import plotly.plotly as py
+import plotly.express as px
 
 def get_list_of_atms():
     #response_atms_json = requests.request("GET","https://api.bankofscotland.co.uk/open-banking/v2.2/atms").json()
@@ -21,28 +24,42 @@ def get_list_of_atms():
 #assert 'eng' in get_list_of_atms()[0]['SupportedLanguages'] # check if first ATM has english as a language
 #print("tests passed")
 
-atm_list = get_list_of_atms()
-atm_num = len( atm_list)
-print("number of atms is: "+str(atm_num))
+def get_rainy_atms_by_region():
+    atm_list = get_list_of_atms()
+    atm_num = len( atm_list)
+    print("number of atms is: "+str(atm_num))
 
-#first, we need to get a list of regions
-regions = []
-region_amount = []
+    #first, we need to get a list of regions
+    regions = []
+    region_amount = []
 
-index = 0
-while index < atm_num:
-    #print("Current index is: "+str(index))
-    current_region = str(atm_list[index]['Location']['PostalAddress']['CountrySubDivision'][0])
-    #print("Region is: "+current_region)
-    if current_region in regions:
-        #print("Region already known")
-        placement = regions.index(current_region)
-        region_amount[placement] = region_amount[placement] + 1
-    else:
-        regions.append(current_region)
-        region_amount.append(1)
-        #print("New Region Added")
-    index = index + 1
+    index = 0
+    while index < atm_num:
+        #print("Current index is: "+str(index))
+        current_region = str(atm_list[index]['Location']['PostalAddress']['CountrySubDivision'][0])
+        branch_subtype = str(atm_list[index]['Location']['LocationCategory'][0])
+        #print("Region is: "+current_region)
+        if current_region in regions:
+            #print("Region already known")
+            placement = regions.index(current_region)
+            if branch_subtype == 'BranchExternal':
+                region_amount[placement] = region_amount[placement] + 1
+        else:
+            regions.append(current_region)
+            if branch_subtype == 'BranchExternal':
+                region_amount.append(1)
+            else:
+                region_amount.append(0)
+            #print("New Region Added")
+        index = index + 1
 
-print(regions)
-print(region_amount)
+    print(regions)
+    print(region_amount)
+    
+    fig = px.bar(x=regions, y=region_amount)
+    fig.show()
+
+#get_rainy_atms_by_region is now finished
+
+
+get_rainy_atms_by_region()
